ed250301f237b31d1142cd9f55d5893bf5fe2cbe
Maxwell <29mlenho04@isd77.org>
2024-12-06 16:48:33 -0600

    Update tutor-bot.py

--- tutor-bot.py
+++ tutor-bot.py
@@ -2,3 +2,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from optimum.intel.neural_compressor import IncQuantizer
 
 model_name="distil-bert-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model  = AutoModelForCausalLM.from_pretrained(model_name)
+
+# Apply dynamic quantization
+quantizer = IncQuantizer(model)
+quantized_model = quantizer.quantize()
+quantized_model.save_pretrained("quantized_model")
