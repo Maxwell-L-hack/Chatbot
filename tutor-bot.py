9d77a9bdf59352023417a8be0dab787e597ff961
Maxwell <29mlenho04@isd77.org>
2024-12-06 16:57:02 -0600

    Added the response generator function

--- tutor-bot.py
+++ tutor-bot.py
@@ -11,3 +11,8 @@ quantized_model = quantizer.quantize()
 quantized_model.save_pretrained("quantized_model")
 
 # Response generator function
+def generate_response(user_input):
+    inputs = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt")
+    outputs = quantized_model.generate(inputs, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(outputs[:, inputs.shade[-1]:][0], skip_special_tokens=True)
+    return response
