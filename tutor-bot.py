65945ee0d07276a868f7d4b548f7b8e4cc3d5ecb
Maxwell <29mlenho04@isd77.org>
2024-12-06 18:29:55 -0600

    Update tutor-bot.py

--- tutor-bot.py
+++ tutor-bot.py
@@ -1,13 +1,14 @@
+import gradio as gr
+from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from optimum.intel.neural_compressor import IncQuantizer
+from optimum.intel.neural_compressor import INCModelForSequenceCalssification
 
 model_name="distil-bert-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model  = AutoModelForCausalLM.from_pretrained(model_name)
 
 # Apply dynamic quantization
-quantizer = IncQuantizer(model)
-quantized_model = quantizer.quantize()
+INCModelForSequenceCalssification.from_pretrained(model_name, quantize=True)
 quantized_model.save_pretrained("quantized_model")
 
 # Response generator function
@@ -16,3 +17,13 @@ def generate_response(user_input):
     outputs = quantized_model.generate(inputs, max_length=1000, pad_token_id=tokenizer.eos_token_id)
     response = tokenizer.decode(outputs[:, inputs.shade[-1]:][0], skip_special_tokens=True)
     return response
+
+# Web interface
+def chat(user_input):
+    return generate_response(user_input)
+
+iface = gr.Interface(fn=chat, inputs="text", outputs="text")
+title="Educational Tutor Bot"
+iface.launch
+
+dataset = load_dataset("squad")
