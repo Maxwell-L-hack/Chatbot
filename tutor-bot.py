55d3eb009120979068d04cb88112760c2b1ed814
Maxwell <29mlenho04@isd77.org>
2024-12-06 18:47:32 -0600

    Update tutor-bot.py

--- tutor-bot.py
+++ tutor-bot.py
@@ -5,7 +5,7 @@ from huggingface_hub import hf_hub_download
 from optimum.intel.neural_compressor import INCModelForSequenceClassification
 
 model_name="distilbert/distilbert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
 model  = AutoModelForMaskedLM.from_pretrained(model_name)
 
 # Apply dynamic quantization
