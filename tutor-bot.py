4b057b0855a698a4ab637eea4d8821694dedef61
Maxwell <29mlenho04@isd77.org>
2024-12-06 19:00:58 -0600

    Update tutor-bot.py

--- tutor-bot.py
+++ tutor-bot.py
@@ -3,6 +3,9 @@ from datasets import load_dataset
 from transformers import AutoModelForMaskedLM, AutoTokenizer
 from huggingface_hub import hf_hub_download
 from optimum.intel.neural_compressor import INCModelForMaskedLM
+from huggingface_hub import hf_hub_download
+
+model_path = hf_hub_download(repo_id=model_name, filename="pytorch_model.bin", force_download=True)
 
 model_name="distilbert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
