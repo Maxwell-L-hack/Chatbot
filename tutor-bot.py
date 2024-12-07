dd5f0da85c5c443314b3f3214e902bc4922ccb9d
Maxwell <29mlenho04@isd77.org>
2024-12-06 18:37:17 -0600

    Update tutor-bot.py

--- tutor-bot.py
+++ tutor-bot.py
@@ -15,7 +15,7 @@ quantized_model.save_pretrained("quantized_model")
 def generate_response(user_input):
     inputs = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt")
     outputs = quantized_model.generate(inputs, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-    response = tokenizer.decode(outputs[:, inputs.shade[-1]:][0], skip_special_tokens=True)
+    response = tokenizer.decode(outputs[:, inputs.shape[-1]:][0], skip_special_tokens=True)
     return response
 
 # Web interface
